af86722256eb9793b03a03a45ddb69ea7c9feef8
A-Little-Excited <3030036858@qq.com>
2024-05-07 17:52:52 +0800

    docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/paion-data/minerva/tree/master/docs',
         },
         theme: {
           customCss: './src/css/custom.css',
