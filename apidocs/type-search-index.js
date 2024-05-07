1fef5ae37342a1f4c5f813eae6b5257264ad3d75
A-Little-Excited <3030036858@qq.com>
2024-05-07 11:07:46 +0000

    deploy: 2b3f5a555d1e326a13999d1a8f1fb7290e09e9fa

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.paiondata.minerva","l":"App"},{"p":"com.paiondata.minerva.config","l":"TestClientConfig"}];updateSearchResults();
